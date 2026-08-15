326832c9cf66f2ef5549607ef18bd422866a15d0
Eric <eriicye@gmail.com>
2026-08-15 17:02:55 +1000

    constants

---
+++ src/constants/Constants.h
@@ -0,0 +1,18 @@
+//
+// Created by Eric on 15/08/2026.
+//
+
+#ifndef CHESS_CONSTANTS_H
+#define CHESS_CONSTANTS_H
+
+struct Constants {
+public:
+    static constexpr int WIDTH = 800;
+    static constexpr int HEIGHT = 600;
+
+    static constexpr char TITLE = *"Chess";
+
+    static constexpr int FPS = 60;
+};
+
+#endif //CHESS_CONSTANTS_H
