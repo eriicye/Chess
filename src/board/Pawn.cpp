56d167f9a269a80cd6fe0acbc7f01fb03ec247c0
Eric <eriicye@gmail.com>
2026-09-19 11:59:21 +1000

    pawn bugs fixed: capturing own pieces + capturing backwards

--- src/board/Pawn.cpp
+++ src/board/Pawn.cpp
@@ -8,6 +8,10 @@
 Pawn::Pawn() = default;
 
 bool Pawn::validMove() {
+    if (isOwnPiece() == true) {
+        return false;
+    }
+
     if (moveBackwards() == true) {
         return false;
     }
@@ -29,6 +33,24 @@ bool Pawn::validMove() {
     }
 
     return true;
+}
+
+bool Pawn::isOwnPiece() {
+    // white
+    if (pieces.board[pieces.old_y][pieces.old_x] > 0) {
+        if (pieces.board[pieces.square_y][pieces.square_x] != 8 && pieces.board[pieces.square_y][pieces.square_x] > 0) {
+            return true;
+        }
+    }
+
+    // black
+    else {
+        if (pieces.board[pieces.square_y][pieces.square_x] != 8 && pieces.board[pieces.square_y][pieces.square_x] < 0) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 bool Pawn::overOneSquare() {
@@ -90,6 +112,3 @@ bool Pawn::differentColumn() {
 
     return false;
 }
-
-
-
